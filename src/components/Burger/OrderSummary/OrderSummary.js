13f2ad88a5e1e7cb8df3ed8c4bd991c217c2d5a4
NikhilOO7 <NikhilOO7@users.noreply.github.com>
2019-02-26 11:13:44 +0530

    Order summary file added

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Aux from "../../../hoc/Aux";
+
+const orderSummary = props => {
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients: </p>
+    </Aux>
+  );
+};
+
+export default orderSummary;
